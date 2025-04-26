9de693ce185f4c3ec92c90955c33b625a0e651ea
Angelina <angelintsuprun@yandex.ru>
2025-04-26 23:43:10 +0700

    попытка перейти на aiogram

--- main.py
+++ main.py
@@ -1,92 +1,49 @@
-import telebot
-from telebot import types, callback_data
-import sqlite3
 import logging
+import sqlite3
+from aiogram import Bot, Dispatcher, types, F
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
-bot = telebot.TeleBot('7280605209:AAH6JW03UpB9qjUf9FRhwwLxlqVTIY7_uGg')
-status_keyboard = telebot.types.InlineKeyboardMarkup().add(
-    telebot.types.InlineKeyboardButton('Ученик', callback_data='1'),
-    telebot.types.InlineKeyboardButton('Учитель', callback_data='2'))
+bot = Bot(token='7280605209:AAH6JW03UpB9qjUf9FRhwwLxlqVTIY7_uGg')
+dp = Dispatcher()
 
+status_buttons = types.InlineKeyboardMarkup()
+pupil = types.InlineKeyboardButton(text='Ученик', callback_data='pupil')
+teacher = types.InlineKeyboardButton(text='Учитель', callback_data='teacher')
+status_buttons.add(pupil, teacher)
 
-@bot.message_handler(commands=['start'])
-def start(message):
+@dp.message(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
     conn = sqlite3.connect('list_of_students.sql')
     cur = conn.cursor()
     cur.execute(
         'CREATE TABLE IF NOT EXISTS users(id int auto_increment primary key, status int, name varchar(100), class varchar(3))')
     conn.commit()
-
-    register = telebot.types.InlineKeyboardMarkup()
-    register.add(telebot.types.InlineKeyboardButton('✍Регистрация', callback_data='users'))
-    bot.send_message(
-        message.chat.id, "🍏Привет, друг\!"
-                         "Ты попал в *«ЛанчБот🥞»* – твоего помощника в школьном питании\!"
-                         "📅*Выбирай завтрак, обед или полдник* – и всё само запишется\."
-                         "📋*Смотри свои заказы* в любое время\."
-                         "⏰*Успевай отменить*, если передумал\!"
-                         "📝*Давай зарегистрируемся\?* Это быстро\!"
-                         "👉Жми *«Регистрация»* и начинай пользоваться\!"
-                         "❓ *Есть вопросы\?* Напиши /help, и я помогу\!",
-        parse_mode='MarkdownV2', reply_markup=register
-    )
-    cur.close()
-    conn.close()
+    await message.answer("🍏Привет, друг!\n"
+                         "Ты попал в *«ЛанчБот🥞»* – твоего помощника в школьном питании!\n"
+                         "📅*Выбирай завтрак, обед или полдник* – и всё само запишется.\n"
+                         "📋*Смотри свои заказы* в любое время.\n"
+                         "⏰*Успевай отменить*, если передумал!\n"
+                         "📝*Давай зарегистрируемся?* Это быстро!\n"
+                         "👉Жми *«Регистрация»* и начинай пользоваться!\n"
+                         "❓ *Есть вопросы?* Напиши /help, и я помогу!\n",
+                         reply_markup=types.InlineKeyboardMarkup().add(
+                             types.InlineKeyboardButton(text='Регистрация', callback_data='register')
+                         ))
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def classification_of_users(call: types.CallbackQuery):
-    bot.send_message(call.message.chat.id, 'Вы учитель или ученик?', reply_markup=status_keyboard)
-    if call.data == '1':
-        logging.info(call.data)
-        # bot.register_next_step_handler(call.message, pupil)
-    if call.data == '2':
-        logging.info(call.data)
-        teacher(call)
+@dp.callback_query(F.text.contains('register'))
+async def registration(callback_query: types.CallbackQuery):
+    await callback_query.message.answer('Вы учитель или ученик?', reply_markup=status_buttons)
 
-# Сделать функции, возращающие значения имени, класса и тп
-
-def pupil(call: types.CallbackQuery):
-    logging.info(call)
-    bot.send_message(call.message.chat.id, 'Назови, пожалуйста, свои ФИО. Хотим познакомиться 👐')
-
-    name = call.message.text.strip()
-    bot.send_message(call.message.chat.id, 'Введи свой класс с литерой класса. Пример: "11А"')
-
-    clas = call.message.text.strip().upper()
-
-    conn = sqlite3.connect('list_of_students.sql')
-    cur = conn.cursor()
-    cur.execute('INSERT INTO users (status, name, class) VALUES ("%s", "%s", "%s")' % ('1', name, clas))
-    conn.commit()
-
-    bot.register_next_step_handler(call.message, main)
+@dp.callback_query(F.text.contains('pupil', 'teacher'))
+async def status_giving(callback_query: types.CallbackQuery):
+    await callback_query.answer('Назови, пожалуйста, свои ФИО. Хотим познакомиться 👐')
 
 
-def teacher(call: types.CallbackQuery):
-    bot.send_message(call.message.chat.id, 'Назовите, пожалуйста, свои ФИО')
 
-    name = call.message.text.strip()
-    bot.send_message(call.message.chat.id, 'Каким классом вы руководите? Напишите цифру и литеру класса. Пример: "11А"')
-
-    clas = call.message.text.strip().upper()
-
-    conn = sqlite3.connect('list_of_students.sql')
-    cur = conn.cursor()
-    cur.execute('INSERT INTO users (status, name, class) VALUES ("%s", "%s", "%s")' % ('2', name, clas))
-    conn.commit()
-
-    bot.register_next_step_handler(call.message, main)
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def main(call: types.CallbackQuery):
-    pass
-
-
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    dp.start_polling(bot)
